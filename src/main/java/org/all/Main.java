4dc2d58068ab1cad20bb11471b633b5662383d22
the-migs <the-migs>
2026-02-28 15:33:05 -0300

    Update 1

--- src/main/java/org/all/Main.java
+++ src/main/java/org/all/Main.java
@@ -1,33 +1,65 @@
 package org.all;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.application.Platform;
-import javafx.stage.Screen;
-import javafx.geometry.Rectangle2D;
-
-import java.awt.*;
+import javafx.util.Duration;
 
 //obrigatorio extender de Application
 public class Main extends Application {
     private boolean captureKey = false;
+    private volatile int repetitions = 0;
+    private volatile int timeBeforePlay = 0;
+    private volatile boolean playMusic = true;
 
     // metodo responsavel por construir tudo
     // Stage stage -> tela inicial do aplicativo
     @Override
     public void start(Stage stage) {
+
+        HBox soundHBox = new HBox();
+        soundHBox.setSpacing(4);
+        soundHBox.setPickOnBounds(false);
+        Button returnButton = new Button("<<");
+        returnButton.getStyleClass().add("button-all");
+        Button playButton = new Button("No dance");
+        playButton.setOnAction(e -> {
+            if (playMusic) {
+                playButton.setText("Dance");
+                playMusic = false;
+            }
+            else {
+                playButton.setText("No dance");
+                playMusic = true;
+            }
+        });
+
+        playButton.getStyleClass().add("button-all");
+        Button skipButton = new Button(">>");
+        skipButton.getStyleClass().add("button-all");
+        soundHBox.getChildren().addAll(returnButton, playButton, skipButton);
         // Layout e container
-        VBox root = new VBox();
+        VBox centerVBOX = new VBox();
+
+        StackPane rootStackPane = new StackPane();
+        StackPane.setAlignment(rootStackPane, Pos.TOP_LEFT);
+
+
         // deixa os componentes no centro
-        root.setAlignment(Pos.CENTER);
+        centerVBOX.setAlignment(Pos.CENTER);
         //espacamento entre os componentes
-        root.setSpacing(20);
+        centerVBOX.setSpacing(20);
         Button setKeyButton = new Button("Set Key: F6");
         // define o estilo do botao com base na classe CSS
         setKeyButton.getStyleClass().add("button-all");
@@ -37,18 +69,51 @@ public class Main extends Application {
             captureKey = true;
         });
 
+        GridPane configGridPane = new GridPane();
+        configGridPane.setAlignment(Pos.CENTER);
+        configGridPane.setPrefSize(500, 100);
+        configGridPane.setMaxSize(500, 100);
+        configGridPane.setVgap(8);
+        configGridPane.setHgap(20);
+        configGridPane.getStyleClass().add("GridPane-all");
+
+        Label repetitionsLabel = new Label("Number of repetitions:");
+        repetitionsLabel.getStyleClass().add("label-all");
+
+        // cria um bloco com seta pra aumentar e diminuir os numeros
+        Spinner<Integer> repetitionsSpinner = new Spinner<>(0, 999999999, 1000);
+        // permite ser editavel
+        repetitionsSpinner.setEditable(true);
+        // pega o valor
+        repetitions = repetitionsSpinner.getValue();
+
+        Label timeBeforePlayLabel = new Label("Seconds before starting:");
+        timeBeforePlayLabel.getStyleClass().add("label-all");
+
+        Spinner<Integer> timeBeforePlaySpinner = new Spinner<>(0, 999999999, 0);
+        timeBeforePlaySpinner.setEditable(true);
+        timeBeforePlay = (timeBeforePlaySpinner.getValue()) * 1000;
+
+        configGridPane.add(repetitionsLabel, 0, 0);
+        configGridPane.add(repetitionsSpinner, 1, 0);
+        configGridPane.add(timeBeforePlayLabel, 0, 1);
+        configGridPane.add(timeBeforePlaySpinner, 1, 1);
+
         Button playAutoClickButton = new Button("Play AutoClick");
         playAutoClickButton.setPrefHeight(50);
         playAutoClickButton.setPrefWidth(200);
         playAutoClickButton.getStyleClass().add("button-all");
-        playAutoClickButton.setOnAction(event -> {});
+        playAutoClickButton.setOnAction(event -> {
+        });
 
         Button stopAutoClickButton = new Button("Stop AutoClick");
         stopAutoClickButton.getStyleClass().add("button-all");
         stopAutoClickButton.setOnAction(event -> {});
+
+        rootStackPane.getChildren().addAll(centerVBOX, soundHBox);
         // scene -> conteudo de dentro da janela, guarda toda a arvore visual
-        // Scene(root, 400, 400) - > define layout e tamanho
-        Scene scene = new Scene(root, 500, 500);
+        // Scene(centerVBOX, 400, 400) - > define layout e tamanho
+        Scene scene = new Scene(rootStackPane, 550, 550);
 
         // vai capturar a tecla precionada, mudar o texto do botao setKey e tornar false a variavel q ativa o proprio if
         scene.setOnKeyPressed(event -> {
@@ -60,28 +125,40 @@ public class Main extends Application {
         });
 
         // Adiciona os botoes
-        root.getChildren().addAll(setKeyButton, playAutoClickButton, stopAutoClickButton);
+        centerVBOX.getChildren().addAll(setKeyButton, playAutoClickButton, stopAutoClickButton, configGridPane);
         // Difine o arquivo css que ditara o estilo e as configuracoes da janela principal
         scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
-        // impede que a tela seja colocada em tela cheia ou aumentada
-        stage.setResizable(false);
+
         // define qual sera o conteudo de dentro da janela
         stage.setScene(scene);
-        // torna a janela principal visivel
+        // impede que a tela seja colocada em tela cheia ou aumentada
+        stage.setResizable(false);
 
-        // Platform.runLater(()
-        // roda o comando abaixo na thread do JavaFX
-        // coloca o codigo na fila do JavaFX
-        // fala pro JavaFX executar quando for seguro
-        Platform.runLater(() -> {
-            // guarda o tamanho da area visivel da tela
-            Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
-            // centraliza o app no centro da tela
-            stage.setX((bounds.getWidth() - stage.getWidth()) / 2);
-            stage.setY((bounds.getHeight() - stage.getHeight()) / 2);
-        });
-
+        // antes de mostrar move a tela para uma area nao visivel
+        // sem isso ela aparece no canto e depois centraliza
+        stage.setX(-100000);
+        stage.setY(-100000);
+        // mesmo que o WM mostre ela, estara transparente
+        stage.setOpacity(0);
+        // pre centralizacao
+        stage.centerOnScreen();
+        // mostra a janela
         stage.show();
+
+        // deixa o JavaFX + WM assentarem tamanho/decoração
+        // espera 20 ms
+        // sem esse delay a centralizacao pode usar um tamanho errado
+        // pode dar micro pulo
+        // pode nao centralizar
+        PauseTransition p = new PauseTransition(Duration.millis(20));
+        // Codigo roda depois que tudo assentou
+        p.setOnFinished(e -> {
+            stage.sizeToScene();
+            stage.centerOnScreen(); // e depois disso tudo, centraliza com tudo preparado
+            stage.setOpacity(1); // janela fica visivel
+        });
+        p.play(); // inicia o timer
+
     }
 
     public static void main(String[] args) {
